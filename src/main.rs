fd7eae13f151a575871cded2f907ae1cf8601152
dubbelosix <dub@06.com>
2022-01-16 13:02:28 +0530

    reduce max backoff elpased to 3 mins

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,15 @@ use {
         retry,
         Error,
         ExponentialBackoff,
+        ExponentialBackoffBuilder,
     },
     core::time::Duration,
-    clap::{Parser},
+    clap::{
+        Arg,
+        App,
+        AppSettings,
+        Parser
+    },
     bs58,
     ureq,
     itertools::izip,
@@ -59,6 +65,7 @@ use {
 const RPC_ENDPOINT: &str = "https://ssc-dao.genesysgo.net/";
 const METADATA_PROGRAM: &str = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";
 const METADATA_CREATOR_ADDRESS_0_OFFSET: usize = 326;
+const FAILED_MARKER: &str = "FAILED";
 
 // setting this high because fetching tokens from the same creator_0 (candymachine) looks like it
 // scans a lot of accounts on the rpc server side. It scans accounts matching a pattern in the
@@ -68,10 +75,10 @@ const METADATA_CREATOR_ADDRESS_0_OFFSET: usize = 326;
 
 const RPC_TIMEOUT: u64 = 300;
 
-fn get_address_filter_for_program(address: String, offset: usize) -> RpcProgramAccountsConfig {
+fn get_address_filter_for_program(address: &String, offset: usize) -> RpcProgramAccountsConfig {
     let memcmp = RpcFilterType::Memcmp(Memcmp{ 
         offset: offset,
-        bytes: MemcmpEncodedBytes::Base58(address),
+        bytes: MemcmpEncodedBytes::Base58(address.to_string()),
         encoding: None,
     });
 
@@ -153,15 +160,18 @@ fn get_token_account_for_mint(mint_addr_str: String)-> String {
         return Err(backoff::Error::Transient{err:"error",retry_after:None});
     };
 
-    let backoff = ExponentialBackoff::default();
+    let backoff = ExponentialBackoffBuilder::default().with_max_elapsed_time(Some(Duration::from_secs(180))).build();
     match retry(backoff, token_account_call) {
         Ok(a) => a,
-        _ => "FAILED".into()
+        _ => FAILED_MARKER.into()
     }
 }
 
 fn get_owner_of_assoc_token(client: &RpcClient, token_account: String) -> String {
     let token_account_owner_call = || {
+        if token_account == FAILED_MARKER {
+            return Err(backoff::Error::Permanent("error"));
+        }
         let rpc_result = client.get_token_account_with_commitment(&Pubkey::new(&bs58::decode(&token_account).into_vec().unwrap())
                                                                   ,CommitmentConfig{commitment:CommitmentLevel::Finalized});
 
@@ -176,19 +186,19 @@ fn get_owner_of_assoc_token(client: &RpcClient, token_account: String) -> String
         }
     };
 
-    let backoff = ExponentialBackoff::default();
+    let backoff = ExponentialBackoffBuilder::default().with_max_elapsed_time(Some(Duration::from_secs(180))).build();
     match retry(backoff, token_account_owner_call) {
         Ok(a) => a,
-        _ => "FAILED".into()
+        _ => FAILED_MARKER.into()
     }
 }
 
-fn get_list_of_mints_in_collection(client: &RpcClient, creator0_address: String) -> Vec<String> {
+fn get_list_of_mints_in_collection(client: &RpcClient, creator0_address: &String) -> Vec<String> {
     let metadata_program_pubkey: Pubkey = Pubkey::new(&bs58::decode(METADATA_PROGRAM).into_vec().unwrap());
     
     let result_vec = client.get_program_accounts_with_config(&metadata_program_pubkey,
                                                              get_address_filter_for_program(
-                                                                 creator0_address,
+                                                                 &creator0_address,
                                                                  METADATA_CREATOR_ADDRESS_0_OFFSET
                                                                  )
                                                              ).unwrap();
@@ -201,18 +211,19 @@ fn get_list_of_mints_in_collection(client: &RpcClient, creator0_address: String)
     mints
 }
 
-fn write_mint_info_to_file(mintrows: Vec<(String,String,String)>) -> std::io::Result<()> {
-    let path = Path::new("newmintfile");
+fn write_mint_info_to_file(creator_address: String, mintrows: Vec<(String,String,String)>) -> std::io::Result<String> {
+    let filename = format!("{}.csv",creator_address);
+    let path = Path::new(&filename);
     let file = File::create(&path)?;
     let mut file = LineWriter::new(file);
-    file.write_all(b"Owner,Mint,Associated Token Account\n")?;
+    file.write_all(b"Mint,Owner,Associated Token Account\n")?;
 
-    for (owner,mint_addr,associated_token_addr) in mintrows {
-        file.write_all(format!("{},{},{}\n",owner,mint_addr,associated_token_addr).as_bytes())?;
+    for (mint_addr,owner,associated_token_addr) in mintrows {
+        file.write_all(format!("{},{},{}\n",mint_addr,owner,associated_token_addr).as_bytes())?;
     }
     file.flush()?;
 
-    Ok(())
+    Ok(filename.into())
 }
 
 fn retry_test_fn()-> Result<String,String> {
@@ -234,22 +245,117 @@ fn retry_test_fn()-> Result<String,String> {
     Ok("rohan".into())
 }
 
+struct FileState {
+    succeeded: Vec<(String,String,String)>,
+    failed: Vec<String>,
+}
+
+fn get_incomplete_mints(fname: String)-> FileState {
+    let mut fs = FileState {succeeded:Vec::<(String,String,String)>::new(), failed:Vec::<String>::new()};
+    let path = Path::new(&fname);
+    let file = File::open(path).expect(&format!("error opening file {}",path.display()));
+    let lines = io::BufReader::new(file).lines();
+    for line in lines {
+        if let Ok(line_content) = line {
+            let lc: Vec<&str> = line_content.split(",").collect();
+            if lc[1] == FAILED_MARKER || lc[2] == FAILED_MARKER {
+                fs.failed.push(lc[0].into());
+            } else {
+                fs.succeeded.push((lc[0].into(),lc[1].into(),lc[2].into()));
+            }
+        }
+    }
+    fs
+}
+
+
+
 fn main() -> std::io::Result<()> {
+    let app = App::new("sol-nft-client")
+        .version("0.1.0")
+        .about("fetch tokens and addresses of an nft collection")
+        .author("dubbelosix")
+        .arg(
+            Arg::new("creator")
+                .short('c')
+                .long("creator")
+                .help("creator address of the collection. (first in creator array)")
+                .required(true)
+                .takes_value(true)
+            )
+        .arg(
+            Arg::new("rpc")
+                .short('r')
+                .long("rpc")
+                .help("rpc url to connect to. defaults to genesysgo mainnet")
+                .takes_value(true)
+                .default_value(RPC_ENDPOINT)
+           )
+        .arg(
+            Arg::new("failed")
+                .short('f')
+                .long("failed")
+                .help("reprocess failed entries from a file")
+            )
+        .arg(
+            Arg::new("threads")
+                .short('t')
+                .long("threads")
+                .takes_value(true)
+                .help("set explicit number of threads")
+            );
 
 
-    let client = RpcClient::new_with_timeout(String::from(RPC_ENDPOINT),
+    let matches = app.get_matches();
+
+    let mut creator_address = String::new();
+    let mut process_failed = false;
+    let mut threads: usize = 20;
+
+    if let Some(i) = matches.value_of("creator"){
+        creator_address = i.to_string();
+    }
+    if matches.is_present("failed") {
+        process_failed = true;
+    }
+    if let Some(i) = matches.value_of("threads"){
+        threads = i.parse().expect("threads must be an integer");
+    }
+
+    let rpc_endpoint = matches.value_of("rpc").unwrap();
+    println!("{},{},{},{}",&creator_address,rpc_endpoint,process_failed,threads);
+
+
+    let fs = get_incomplete_mints("newmintfile.csv".into());
+    for i in fs.failed {
+        println!("{}",i);
+    }
+
+    rayon::ThreadPoolBuilder::new().num_threads(threads).build_global().unwrap();
+
+
+
+    let client = RpcClient::new_with_timeout(String::from(rpc_endpoint),
                                         Duration::from_secs(RPC_TIMEOUT));
 
     println!("Getting mints");
-    let mint_list = get_list_of_mints_in_collection(&client, "DVemJ8n9ZiSmSf8a18VYgpBgTUoHEA8x6ZZBsTL2bxk9".into());
+    let mint_list = get_list_of_mints_in_collection(&client, &creator_address);
+    if mint_list.len() == 0 {
+        println!("No mints for creator address: {}", creator_address);
+        return Ok(());
+    }
     println!("Getting associated token account list");
     let pb = ProgressBar::new(mint_list.len() as u64);
     let token_account_list : Vec<String> = mint_list.par_iter().progress_with(pb).map(|x| get_token_account_for_mint(x.into())).collect();
+
     println!("Getting associated token account owners");
     let pb = ProgressBar::new(mint_list.len() as u64);
-    let token_account_owner_list: Vec<String> = token_account_list.par_iter().progress_with(pb).map(|x| get_owner_of_assoc_token(&client,x.into())).collect();
-    let row_vec: Vec<(String,String,String)> = izip!(token_account_owner_list,mint_list,token_account_list).map(|(x,y,z)| (x,y,z)).collect(); 
-    write_mint_info_to_file(row_vec);
+    let token_account_owner_list: Vec<String> = token_account_list.par_iter().progress_with(pb).map(|x| get_owner_of_assoc_token(&client, x.into())).collect();
+
+    println!("Formatting final results...");
+    let row_vec: Vec<(String,String,String)> = izip!(mint_list,token_account_owner_list,token_account_list).map(|(x,y,z)| (x,y,z)).collect(); 
+    let fname = write_mint_info_to_file(creator_address.into(),row_vec).unwrap();
+    println!("results in file: {}",fname);
 
     Ok(())
 }
